d86464d51757f4ba4e8dcc56dfdd7c1d641fa2bd
cristiellen <cristiellenalvess@gmail>
2021-06-18 00:04:48 -0400

    remocao com foreignkey

--- CadastrarAnimal/app/src/main/java/com/tcc/invernada/activity/ListarInvernadaActivity.java
+++ CadastrarAnimal/app/src/main/java/com/tcc/invernada/activity/ListarInvernadaActivity.java
@@ -6,10 +6,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import com.tcc.fazenda.dao.Fazenda;
 import com.tcc.invernada.dao.Invernada;
 import com.tcc.main.MainActivity;
@@ -22,6 +25,7 @@ import java.util.ArrayList;
 
 public class ListarInvernadaActivity extends AppCompatActivity {
     private ArrayList<Invernada> invernadas;
+    private Invernada invernada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +62,16 @@ public class ListarInvernadaActivity extends AppCompatActivity {
 
         ListView listaView = (ListView) findViewById(R.id.lista);
         listaView.setOnItemClickListener(this::onItemClick);
+        listaView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            //vai chamar o menu criado na linha 93
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                invernada = invernadas.get(position);//pegando o elemento
+                return false;
+            }
+        });
+        registerForContextMenu(listaView); //habilitando menu de contexto(clicar e segurar pra abrir opções)
+
 
         ArrayAdapter<Invernada> adapter = new ArrayAdapter<Invernada>(this, android.R.layout.simple_list_item_1, newList );
         listaView.setAdapter(adapter);
@@ -80,6 +94,29 @@ public class ListarInvernadaActivity extends AppCompatActivity {
         Invernada.setId_temp(invernadas.get(position).getId());
         startActivity(intent);
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem menuItem = menu.add("Deletar"); //opção do menu que vai aparecer
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                invernada.fazenda.setTarget(null);
+                ObjectBox.get().boxFor(Invernada.class).remove(invernada);
+                Intent intent = new Intent(v.getContext(), ListarInvernadaActivity.class);
+                alerta("Invernada '"+invernada.getNome()+"' removida");
+                startActivity(intent);
+                return false;
+            }
+        });
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    private void alerta(String mensagem){
+        //funcao pra exibir mensagem básica ao usuario
+        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show();
+    }
+
 
     //TODO: PRIMEIRO LISTAR E CADASTRAR A APARECER DEVE SER O DA FAZENDA
     //TODO: LISTAR OS BEBEDOUROS E NESSA TELA MOSTRAR UM BOTÃO CADASTRO
